7b2163bf83aed667a0e2520df8329d88ec22adb7
santiagopalma <san_tiago96@hotmail.com>
2021-06-10 16:51:29 -0500

    codeinspections correct

--- src/main/java/labcodeinspection/Email.java
+++ src/main/java/labcodeinspection/Email.java
@@ -1,25 +1,35 @@
 package labcodeinspection;
-
+import java.util.Locale;
 public class Email {
 
-	private String m_firstName;
-	private String m_lastName;
-	private String password = null;
-	private String department;
-	private int defaultpasswordLength = 8;
-	private String email;
-
-	public Email(String firstName, String lastName) {
-		this.m_firstName = firstName;
-		this.m_lastName = lastName;
+	final transient private String mfirstName;
+	final transient private String mlastName;
+	transient private String password;
+	transient private String department;
+	final transient private int defpasswordLength = 8;
+	transient private String email;
+	
+	/**
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 */
+	public Email(final String firstName,final String lastName) {
+		this.mfirstName = firstName;
+		this.mlastName = lastName;
 	}
-
+	/**
+	 * 
+	 */
 	public void showInfo() {
-		System.out.println("\nFIRST NAME= " + m_firstName + "\nLAST NAME= " + m_lastName);
+		System.out.println("\nFIRST NAME= " + mfirstName + "\nLAST NAME= " + mlastName);
 		System.out.println("DEPARMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password);
 	}
-
-	public void setDeparment(int depChoice) {
+	/**
+	 * 
+	 * @param depChoice
+	 */
+	public void setDeparment(final int depChoice) {
 		switch (depChoice) {
 		case 1:
 			this.department = "sales";
@@ -30,22 +40,26 @@ public class Email {
 		case 3:
 			this.department = "acct";
 			break;
+		default:
+			break;
 		}
 	}
 
-	private String randomPassword(int length) {
-		String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
+	private String randomPassword(final int length) {
+		final String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
 		char[] password = new char[length];
 		for (int i = 0; i < length; i++) {
-			int rand = (int) (Math.random() * set.length());
+			final int rand = (int) (Math.random() * set.length());
 			password[i] = set.charAt(rand);
 		}
 		return new String(password);
 	}
-
+	/**
+	 *  
+	 */
 	public void generateEmail() {
-		this.password = this.randomPassword(this.defaultpasswordLength);
-		this.email = this.m_firstName.toLowerCase() + this.m_lastName.toLowerCase() + "@" + this.department
+		this.password = this.randomPassword(this.defpasswordLength);
+		this.email = this.mfirstName.toLowerCase(Locale.US) + this.mlastName.toLowerCase(Locale.US) + "@" + this.department
 				+ ".espol.edu.ec";
 	}
 }
--- src/main/java/labcodeinspection/EmailApp.java
+++ src/main/java/labcodeinspection/EmailApp.java
@@ -2,25 +2,37 @@ package labcodeinspection;
 
 import java.util.Scanner;
 
+/**
+ * 
+ * @author san_t
+ *
+ */
 public class EmailApp {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	private EmailApp(){
+		
+	}
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		final Scanner scanner = new Scanner(System.in);
 
 		System.out.print("Enter your first name: ");
-		String firstName = sc.nextLine();
+		final String firstName = scanner.nextLine();
 
 		System.out.print("Enter your last name: ");
-		String lastName = sc.nextLine();
+		final String lastName = scanner.nextLine();
 
 		System.out.print("\nDEPARTMENT CODE\n1. for sales\n2. for Development\n3. for accounting\nEnter code: ");
 
-		int depChoice = sc.nextInt();
-		sc.close();
+		final int depChoice = scanner.nextInt();
+		scanner.close();
 
-		Email email = new Email(firstName, lastName);
+		final Email email = new Email(firstName, lastName);
 		email.setDeparment(depChoice);
 		email.generateEmail();
 		email.showInfo();
 	}
+	
 }
